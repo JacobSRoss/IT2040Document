de245ca059b2e5832905cb01ab20e0d9a45e8638
Jacob Ross <jross1818@gmail.com>
2018-09-14 19:51:44 -0500

    finished challenge, working application that creates documents dynamically

--- DocumentChallenge/Program.cs
+++ DocumentChallenge/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DocumentChallenge
 {
@@ -10,6 +11,30 @@ namespace DocumentChallenge
     {
         static void Main(string[] args)
         {
+            String documentName;
+            String documentContent;
+            int characterCount = 0;
+
+            Console.WriteLine("Document");
+            Console.WriteLine("");
+            Console.WriteLine("Name of the document?: ");
+            documentName = Console.ReadLine();
+            Console.WriteLine("Contents of the document?: ");
+            documentContent = Console.ReadLine();
+
+            try{
+                documentName += ".txt";
+                using (StreamWriter sw = new StreamWriter(documentName))
+                {
+                    sw.WriteLine(documentContent);
+                }
+            } catch (Exception e){
+                Console.WriteLine("Exception has occured");
+                //Environment.Exit;
+            }
+            characterCount = documentContent.Length;
+            Console.WriteLine(documentName +" was successfully saved. The document contains " + characterCount + " characters.");
+            Console.ReadLine();
         }
     }
 }
